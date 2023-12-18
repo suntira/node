4292e8d5208255be667b57a1ea91f72519c4c364
Pasinkova Irina <suntunartist@mail.ru>
2023-12-18 20:00:21 +0300

    dcdx

--- public/js/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express()
-const  bodyParser = require('body-parser');
-const urlencoded = bodyParser.urlencoded({extended: false});
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize("BBdd", "root", "", {
-    dialect: 'mysql',
-    host: 'localhost',
-});
